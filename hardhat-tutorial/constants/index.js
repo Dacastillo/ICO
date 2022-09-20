ecc90aa380d1ce6a72fe1cca0f3c2ad886d947fb
Dacastillo <dacastillo@uc.cl>
2022-09-20 13:21:43 -0300

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x7A8B2d56A68CE9FEe32b2C3b07591788B950046e";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
